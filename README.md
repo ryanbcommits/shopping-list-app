ed3d15696ae55e68831656129f6f5a63e1397a72
Ryan <ryan.burns01@gmail.com>
2025-05-31 15:37:48 -0400

    Starting with a barebones html to test bugs with a db

---
+++ README.md
@@ -0,0 +1,3 @@
+##Connecting the frontend to a DB
+I solely want to establish a connection to the vsCode-2025 db I created.
+After that is successful I want to submit data to it after.
---
+++ script.js
@@ -0,0 +1,8 @@
+// get the button by its ID
+const button = document.getElementById("connect"); // remember the element id has to mathch the one in the HTML with the same name.
+
+//add an event listener to the button
+button.addEventListener('click', function() {
+    console.log('Button was clicked!');
+});
+
