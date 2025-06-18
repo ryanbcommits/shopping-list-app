3b42ec41280c27ae16aa8927413b7f3e41983c4a
Ryan <mr.ryan.burns@outlook.com>
2025-06-17 22:16:42 -0400

    Happy with how things are working - still need to set up my sign up page though. Probably need to download Firebase sdk

--- login.js
+++ login.js
@@ -6,40 +6,37 @@ import { auth } from './firebase-config.js';
 // DOMContentLoaded ensures DOM elements exist before we try to access them
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('loginForm');
-
-    
     // sign up code (works if above the login code?)
     // Get the modal
     const modal = document.getElementById("myModal"); 
     const btn = document.getElementById("myBtn");  
-    
     // Get the <span> element that closes the modal
     const span = document.getElementsByClassName("close")[0];
-    
-    //getElementById("close")[0];
 
     // When the user clicks the button, open the modal
-    // error is being thrown
     btn.onclick = function() {
         modal.style.display = "block";
     }
-
 
     //When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
     }
 
-
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    
     loginForm.addEventListener("submit", async (e) => {
         e.preventDefault();
-
 
         const loginEmail = document.getElementById("userEmail").value;
         const password = document.getElementById("password").value;
     try {
             const userCredential = await signInWithEmailAndPassword(auth, loginEmail, password);
-            console.log("login successful!", userCredential.loginEmail);
+            console.log("login successful!", userCredential.user.email);
     
             alert("Login Successful!");
 
