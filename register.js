ee8c7749474dbf1fedd5787e378a3d699e414f72
Ryan <mr.ryan.burns@outlook.com>
2025-06-25 22:48:47 -0400

    Adding Firebase foundational code to register.js

--- register.js
+++ register.js
@@ -2,4 +2,23 @@ import {auth} from  './firebase-config';
 
 import { createUserWithEmailAndPassword } from 'firebas/auth'
 
+/*
+Source of methods are from Firebase documentation to create a user 
+and also set something called a state observer and get user data...
+see:  https://firebase.google.com/docs/auth/web/start#web_2
+*/
+// Sign up new users
 
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+  // Set an authentication state observer and get user Data
